bc4212585bf55da30431be986b8c76aa8e67d028
堀巧 <horitakumi@horitakuminoMacBook-Pro.local>
2024-09-23 16:51:18 -0700

    UPDATE to add validation on ProcessReceiptService

--- app/src/main/kotlin/com/receiptProcessor/receiptProcessor/services/ProcessReceiptService.kt
+++ app/src/main/kotlin/com/receiptProcessor/receiptProcessor/services/ProcessReceiptService.kt
@@ -1,17 +1,22 @@
 package com.receiptProcessor.receiptProcessor.services
 
+import com.receiptProcessor.receiptProcessor.exceptions.InvalidArgumentException
 import com.receiptProcessor.receiptProcessor.models.ProcessReceiptItem
 import com.receiptProcessor.receiptProcessor.models.ReceiptId
 import com.receiptProcessor.receiptProcessor.repositories.ReceiptRepository
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
 object ProcessReceiptService {
     fun execute(params: ProcessReceiptParams): ReceiptId {
         return ReceiptRepository.create(
-            retailer = params.retailer,
-            purchaseDate = params.purchaseDate,
-            purchaseTime = params.purchaseTime,
-            items = params.items.map { it.toModel() },
-            total = params.total.toDouble(),
+            retailer = params.validRetailer(),
+            purchaseDate = params.validatePurchaseDate(),
+            purchaseTime = params.validatePurchaseTime(),
+            items = params.validateItems(),
+            total = params.validateTotal(),
         )
     }
 }
@@ -22,7 +27,38 @@ data class ProcessReceiptParams(
     val purchaseTime: String,
     val items: List<ProcessReceiptItemParams>,
     val total: String,
-)
+) {
+    fun validRetailer(): String =
+        this.retailer.ifEmpty { throw InvalidArgumentException("retailer is invalid") }
+
+    fun validatePurchaseDate(): String {
+        try {
+            val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+            LocalDate.parse(purchaseDate, formatter)
+            return this.purchaseDate
+        } catch (e: DateTimeParseException) {
+            throw InvalidArgumentException("purchaseDate is invalid")
+        }
+    }
+
+    fun validateItems(): List<ProcessReceiptItem> =
+        if (this.items.isNotEmpty())
+            this.items.map { it.toModel() }
+        else
+            throw InvalidArgumentException("items is invalid")
+
+    fun validatePurchaseTime(): String {
+        try {
+            LocalTime.parse(purchaseTime)
+            return this.purchaseTime
+        } catch (e: DateTimeParseException) {
+            throw InvalidArgumentException("purchaseTime is invalid")
+        }
+    }
+
+    fun validateTotal(): Double =
+        this.total.toDoubleOrNull() ?: throw InvalidArgumentException("total is invalid")
+}
 
 data class ProcessReceiptItemParams(
     val shortDescription: String,
