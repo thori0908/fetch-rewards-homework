d9f3749e6dd427f0d7be983e5ba6e5435924c255
堀巧 <horitakumi@horitakuminoMacBook-Pro.local>
2024-09-23 13:01:36 -0700

    UPDATE GlobalExceptionHandler.handleNotFound

--- app/src/main/kotlin/com/receiptProcessor/receiptProcessor/controllers/GlobalExceptionHandler.kt
+++ app/src/main/kotlin/com/receiptProcessor/receiptProcessor/controllers/GlobalExceptionHandler.kt
@@ -2,6 +2,7 @@ package com.receiptProcessor.receiptProcessor.controllers
 
 import com.receiptProcessor.receiptProcessor.exceptions.ResourceNotFoundException
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -10,7 +11,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 object GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException::class)
-    fun handleNotFound(exception: ResourceNotFoundException): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.message)
+    fun handleNotFound(exception: ResourceNotFoundException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(ErrorResponse.of(exception))
+    }
+}
+
+data class ErrorResponse(val message: String?) {
+    companion object {
+        fun of(notFoundException: ResourceNotFoundException): ErrorResponse =
+            ErrorResponse(notFoundException.message)
     }
 }
